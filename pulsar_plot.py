a6ae7288a79c1d29d0e01ee173cb814c3a1f3486
lampochki <lampochki@gmail.com>
2017-12-13 18:58:35 +0300

    searsh for the maximum snr

--- pulsar_plot.py
+++ pulsar_plot.py
@@ -103,24 +103,34 @@ def create(fn='', bn='', pp='', fi='', outfile='', per=''):
 	print data.shape
 	print data
 
-def snr_1D(fn='', per='', aver='', sect=''):
-	data=np.load(fn)[per,0:]
-	average=data.shape[0]/aver
-	aver_data=np.zeros((aver,64), dtype=np.complex)
-	for x1 in range (aver):
-			for x in range (average):
-				aver_data[x1,0:]+=data[x1+aver*x,0:]
-	ifft_data=abs(fft.fftshift(fft.ifft(aver_data)))
-	section=ifft_data[sect,0:]
-	section=section.tolist()
-	A=max(section)
-	index_from=section.index(A)-5
-	index_to=section.index(A)+5
-	del section[index_from:index_to]
-	average=np.mean(section)
-	st_deviation=np.std(section)
-	SNR=(A-average)/st_deviation
-	return SNR
+#pulsar_plot.snr_1D('filname.npy', 63, 42)
+def snr_1D(fn='', aver='', sect=''):
+	periods=aver
+	SNR_new=1
+	SNR_old=0
+	SNR=0
+	while SNR_old<SNR_new:
+		data=np.load(fn)[0:periods,0:]
+		average=data.shape[0]/aver
+		aver_data=np.zeros((aver,64), dtype=np.complex)
+		for x1 in range (aver):
+				for x in range (average):
+					aver_data[x1,0:]+=data[x1+aver*x,0:]
+		ifft_data=abs(fft.fftshift(fft.ifft(aver_data)))
+		section=ifft_data[sect,0:]
+		section=section.tolist()
+		A=max(section)
+		index_from=section.index(A)-5
+		index_to=section.index(A)+5
+		del section[index_from:index_to]
+		average=np.mean(section)
+		st_deviation=np.std(section)
+		SNR_old=SNR
+		SNR=(A-average)/st_deviation
+		SNR_new=SNR
+		periods+=aver
+		print SNR
+	return SNR_old
 
 	
 
