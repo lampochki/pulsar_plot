2de3e5ce58f816edf1172c21a1b91d5fa4affca1
lampochki <lampochki@gmail.com>
2017-12-14 17:55:30 +0300

    try make 3d

--- pulsar_plot.py
+++ pulsar_plot.py
@@ -5,6 +5,9 @@ import matplotlib.pyplot as plt
 import numpy.fft as fft
 import pylab
 from mpl_toolkits.mplot3d import Axes3D
+import plotly.plotly as py
+import plotly.graph_objs as go
+from plotly import tools
 
 
 #pulsar_plot.create('DIFX_56341_040423.s0000.b0000', 258, 'RR', 0, 'filname',0)
@@ -200,7 +203,6 @@ def plot(fn='', aver='',periods='', outfile=''):
  	plt.ylabel('accumulation periods')
  	pylab.show()
  	np.save(outfile, ifft_data)
-	
 
 def plot2D(fn='',s=''):
 	ifft_data=np.load(fn)
@@ -209,5 +211,60 @@ def plot2D(fn='',s=''):
 	plt.plot(section)
 	plt.show()
 
-	
+def plots(fn='', aver='',periods=''):
+	data=np.load(fn)
+	for aver in range (60,70):
+		average=data.shape[0]/aver
+		aver_data=np.zeros((aver,64), dtype=np.complex)
+		for x1 in range (aver):
+				for x in range (average):
+					aver_data[x1,0:]+=data[x1+aver*x,0:]
+		ifft_data=abs(fft.fftshift(fft.ifft(aver_data)))
+		
+		x = np.arange (0, 64, 1)
+ 		y = np.arange (0, aver, 1)
+ 		xgrid, ygrid = np.meshgrid(x, y)
+ 		zgrid = ifft_data
+ 		x, y, z = xgrid,ygrid,zgrid
+ 		
+ 		fig = pylab.figure()
+ 		axes = Axes3D(fig)
+ 		axes.plot_surface(x, y, z)
+ 		plt.xlabel('delay')
+ 		plt.ylabel('accumulation periods')
+ 		pylab.show()
+ 		np.save(outfile, ifft_data)
 
+def testplot(fn=''):
+	data=np.load(fn)
+	fig = plt.figure(figsize=plt.figaspect(0.5))
+	aver=64
+	aver1=65
+	average=data.shape[0]/aver
+	average1=data.shape[0]/aver1
+	new_data=np.zeros((aver,64), dtype=np.complex)
+	new_data1=np.zeros((aver1,64), dtype=np.complex)
+	for x1 in range (aver):
+ 			for x in range (average):
+ 				new_data[x1,0:]+=data[x1+aver*x,0:]
+ 	ifft_data=abs(fft.fftshift(fft.ifft(new_data)))
+ 	ax = fig.add_subplot(1, 2, 1, projection='3d')
+ 	x = np.arange (0, 64, 1)
+ 	y = np.arange (0, aver, 1)
+ 	xgrid, ygrid = np.meshgrid(x, y)
+ 	zgrid = ifft_data
+ 	x, y, z = xgrid,ygrid,zgrid
+ 	surf=ax.plot_surface(x,y,z)
+
+ 	for x1 in range (aver1):
+ 			for x in range (average1):
+ 				new_data1[x1,0:]+=data[x1+aver1*x,0:]
+ 	ifft_data=abs(fft.fftshift(fft.ifft(new_data)))
+ 	ax = fig.add_subplot(1, 2, 2, projection='3d')
+ 	x = np.arange (0, 64, 1)
+ 	y = np.arange (0, aver1, 1)
+ 	xgrid, ygrid = np.meshgrid(x, y)
+ 	zgrid = ifft_data
+ 	q, w, f = xgrid,ygrid,zgrid
+ 	surf=ax.plot_surface(q,w,f)
+ 	plt.show()
